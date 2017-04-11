3d69f512df8840fa0b91848376ebf7d4c6e5e4d5
JoaoBraveCoding <joao.marcal12@hotmail.com>
2017-04-11 20:41:23 +0100

    Added BIT files

---
+++ BIT/samples/ICount.java
@@ -0,0 +1,70 @@
+	/* ICount.java
+ * Sample program using BIT -- counts the number of instructions executed.
+ *
+ * Copyright (c) 1997, The Regents of the University of Colorado. All
+ * Rights Reserved.
+ * 
+ * Permission to use and copy this software and its documentation for
+ * NON-COMMERCIAL purposes and without fee is hereby granted provided
+ * that this copyright notice appears in all copies. If you wish to use
+ * or wish to have others use BIT for commercial purposes please contact,
+ * Stephen V. O'Neil, Director, Office of Technology Transfer at the
+ * University of Colorado at Boulder (303) 492-5647.
+ */
+
+import BIT.highBIT.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+
+public class ICount {
+    private static PrintStream out = null;
+    private static String template = "";
+
+    /* main reads in all the files class files present in the input directory,
+     * instruments them, and outputs them to the specified output directory.
+     */
+    public static void main(String argv[]) {
+        File file_in = new File(argv[0]);
+        String infilenames[] = file_in.list();
+
+        for (int i = 0; i < infilenames.length; i++) {
+            String infilename = infilenames[i];
+            if (infilename.startsWith("Main.class") || infilename.startsWith("RayTracer.class")) {
+                // create class info object
+                ClassInfo ci = new ClassInfo(argv[0] + System.getProperty("file.separator") + infilename);
+
+                // loop through all the routines
+                // see java.util.Enumeration for more information on Enumeration class
+                for (Enumeration e = ci.getRoutines().elements(); e.hasMoreElements(); ) {
+                    Routine routine = (Routine) e.nextElement();
+                    routine.addBefore("ICount", "methodIn", routine.getMethodName());
+                    routine.addAfter("ICount", "methodOut", routine.getMethodName());
+                }
+
+                ci.addAfter("ICount", "writeToFile", "Ola");
+
+                ci.write(argv[1] + System.getProperty("file.separator") + infilename);
+            }
+        }
+    }
+
+    public static synchronized void methodIn(String methodName) {
+        template += "Entrei no metodo " + methodName + " Sou a thread " + Thread.currentThread().getId() + "\n";
+    }
+
+    public static synchronized void methodOut(String methodName) {
+        template += "Sai do metodo " + methodName + " Sou a thread " + Thread.currentThread().getId() + "\n";
+    }
+
+    public static synchronized void writeToFile(String ola) {
+        try {
+            Files.write(Paths.get("metadata.in"), template.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
