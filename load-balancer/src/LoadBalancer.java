a0d88e019eb87375e2dedaeb393c9e126b509279
JoaoBraveCoding <joao.marcal12@hotmail.com>
2017-05-18 13:14:05 +0100

    Fixed a bug Bug we were not removing old instances for the map with threads

--- load-balancer/src/LoadBalancer.java
+++ load-balancer/src/LoadBalancer.java
@@ -46,7 +46,7 @@ public class LoadBalancer {
     private static HashMap<Instance, Integer> instanceActiveThreads;
 
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         init();
         initDatabase();
         System.out.println(instances.size());
@@ -192,6 +192,12 @@ public class LoadBalancer {
                         removeInstanceActiveThreads(instance);
                     }
                 }
+            }
+        }
+
+        for (Instance instance : getInstanceActiveThreads().keySet()) {
+            if (!instances.contains(instance)){
+                removeInstanceActiveThreads(instance);
             }
         }
 
